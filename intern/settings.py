1b2e43b6feaf070e6ad7b6ad6d736565e73003e5
sanjay <sanjaykumarchourasiya12@gmail.com>
2022-04-26 14:46:59 +0530

    Set Debug based on envirnment variables.

--- intern/settings.py
+++ intern/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -143,3 +144,8 @@ REST_FRAMEWORK = {
 # Heroku settings.
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
